3fd0aaae39fc7d6a6ebe6fcf1b0c9e86ee1d0c44
Chase Brasfield <chasebrasfield@gmail.com>
2016-04-11 17:22:44 -0500

    Mobil nav modal is working now

--- app/src/templatecontrols/navbar/navbar.tc.ts
+++ app/src/templatecontrols/navbar/navbar.tc.ts
@@ -2,6 +2,23 @@ import {register, ui} from 'platypus';
 
 export default class NavbarTemplateControl extends ui.TemplateControl {
     templateString: string = require('./navbar.tc.html');
+    hasOwnContext = true;
+    
+    context: any = {
+        modal: false,
+    };
+    
+    toggleModal() {
+        if (this.context.modal === false) {
+           this.context.modal = true; 
+        } else {
+            this.context.modal = false;
+        }
+        
+    }
+    
 }
+
+
 
 register.control('navbar', NavbarTemplateControl);
