d33c5001f510f0a1265c84cafa2dfd0675a9b886
Bcaskey <bcaskeyu@charter.net>
2016-04-11 21:26:33 -0500

    adding hero and plat-visible func

--- app/src/viewcontrols/howtoplay/howtoplay.vc.ts
+++ app/src/viewcontrols/howtoplay/howtoplay.vc.ts
@@ -4,7 +4,23 @@ import BaseViewControl from '../base/base.vc';
 export default class HowtoplayViewControl extends BaseViewControl {
     templateString: string = require('./howtoplay.vc.html');
 
-    context: any = {};
+    context: any = {
+        showMe: true,
+        dontShowMe: false
+    };
+    
+    showMe(): void {
+        this.context.showMe = true;
+        this.context.dontShowMe = false;
+    }
+    
+    dontShowMe(): void {
+        this.context.showMe = false;
+        this.context.dontShowMe = true;
+    }
 }
 
 register.viewControl('howtoplay-vc', HowtoplayViewControl);
+
+// showMe()
+// dontShowMe()
