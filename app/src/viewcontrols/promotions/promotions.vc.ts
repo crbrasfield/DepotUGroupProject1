08ad04149556de865093fc0786267b860b467d79
LaMonte <austinlamonte@yahoo.com>
2016-04-13 12:26:28 -0500

    promo commit

--- app/src/viewcontrols/promotions/promotions.vc.ts
+++ app/src/viewcontrols/promotions/promotions.vc.ts
@@ -4,7 +4,28 @@ import BaseViewControl from '../base/base.vc';
 export default class PromotionsViewControl extends BaseViewControl {
     templateString: string = require('./promotions.vc.html');
 
-    context: any = {};
+
+    context: any = {
+        modal: false,
+        modalMobile: false
+    };
+    
+    toggleModal() {
+        if (this.context.modal == false) {
+        this.context.modal = true;
+        } else {
+            this.context.modal = false;
+        }
+    }
+     toggleModalMobile() {
+        if (this.context.modalMobile == false) {
+        this.context.modalMobile = true;
+        } else {
+            this.context.modalMobile = false;
+        }
+    }
 }
+
+
 
 register.viewControl('promotions-vc', PromotionsViewControl);
